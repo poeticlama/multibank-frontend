2dfd525df4d495b16bd1f8bd2848f73259e33709
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-10-29 15:09:24 +0300

    chore: add boilerplate

---
+++ src/store/index.ts
@@ -0,0 +1,24 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        value: 0
+    },
+    reducers: {
+        incremented: state => {
+            state.value += 1
+        },
+        decremented: state => {
+            state.value -= 1
+        }
+    }
+})
+
+export const { incremented, decremented } = counterSlice.actions
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
+
+export default store
