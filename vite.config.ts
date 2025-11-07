03ad62e0111e54d71f2fa18664ebbc824728a2d2
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-11-07 15:23:09 +0300

    build: add dev Dockerfile and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {fileURLToPath, URL} from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+    },
+    proxy: {
+      '/api': {
+        target: 'http://backend:8080',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    },
+  },
   plugins: [
       react(),
       tailwindcss(),
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
 })
