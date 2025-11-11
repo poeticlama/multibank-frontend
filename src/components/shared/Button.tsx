83759b4010d7466293be3f82e7cc58f48da3c158
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-11-12 02:06:28 +0300

    fix: add cursor-pointer parameter

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -6,6 +6,7 @@ type ButtonProps = {
   onClick: () => void;
   variant?: 'primary' | 'secondary';
   disabled?: boolean;
+  pointer?: boolean;
 };
 
 export const Button = ({
@@ -13,6 +14,7 @@ export const Button = ({
   onClick,
   variant = 'primary',
   disabled = false,
+  pointer = false,
 }: ButtonProps) => {
   const baseStyles =
     'w-full py-3 px-6 rounded-lg font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2';
@@ -20,13 +22,13 @@ export const Button = ({
   const variants = {
     primary: `${
       disabled
-        ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
-        : 'bg-blue-900 text-white hover:bg-blue-700 focus:ring-blue-500'
+        ? `bg-gray-300 text-gray-500 cursor-not-allowed ${pointer? 'cursor-pointer' : ''}`
+        : `bg-blue-900 text-white hover:bg-blue-700 focus:ring-blue-500 ${pointer? 'cursor-pointer' : ''}`
     }`,
     secondary: `${
       disabled
-        ? 'bg-gray-200 text-gray-500 cursor-not-allowed'
-        : 'bg-gray-100 text-gray-700 hover:bg-gray-200 focus:ring-gray-500'
+        ? `bg-gray-200 text-gray-500 cursor-not-allowed ${pointer? 'cursor-pointer' : ''}`
+        : `bg-gray-100 text-gray-700 hover:bg-gray-200 focus:ring-gray-500 ${pointer? 'cursor-pointer' : ''}`
     }`,
   };
 
