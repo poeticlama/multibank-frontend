2676a2ac367485612d34f7408eaae550c4db679a
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-11-15 00:40:24 +0300

    chore: remove mocks

--- src/mocks/users-mock.ts
+++
@@ -1,9 +0,0 @@
-import type { User } from '../types/account-types.ts';
-
-const mockUsers: User[] = [
-  { id: 1, login: 'user1', password: '1111', name: 'Пенис Денисович', status: 'DEFAULT' },
-  { id: 2, login: 'user2', password: '2222', name: 'Иван Говнов', status: 'DEFAULT' },
-  { id: 3, login: 'admin', password: 'admin123', name: 'Админисратор', status: 'PREMIUM' },
-];
-
-export default mockUsers;
